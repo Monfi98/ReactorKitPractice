a287e1fa542765d0e2965ac62ca72766dcd91f15
shinseungjae <mok05247@naver.com>
2024-10-07 10:05:09 +0900

    적용 전 뷰 그리기

--- ReactorKitPractice/ViewController.swift
+++ ReactorKitPractice/ViewController.swift
@@ -6,15 +6,76 @@
 //
 
 import UIKit
+import ReactorKit
+import RxCocoa
+import RxSwift
 
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Property
+    let decreaseButton: UIButton = {
+        let decreaseButton = UIButton()
+        decreaseButton.setImage(UIImage(systemName: "minus"), for: .normal)
+        decreaseButton.tintColor = .black
+        decreaseButton.translatesAutoresizingMaskIntoConstraints = false
+        return decreaseButton
+    }()
+    
+    let increaseButton: UIButton = {
+        let increaseButton = UIButton()
+        increaseButton.setImage(UIImage(systemName: "plus"), for: .normal)
+        increaseButton.tintColor = .black
+        increaseButton.translatesAutoresizingMaskIntoConstraints = false
+        return increaseButton
+    }()
+    
+    let valueLabel: UILabel = {
+        let valueLabel = UILabel()
+        valueLabel.text = "0"
+        valueLabel.translatesAutoresizingMaskIntoConstraints = false
+        return valueLabel
+    }()
+    
+    let activityIndicator: UIActivityIndicatorView = {
+        let statusIndicator = UIActivityIndicatorView()
+        statusIndicator.translatesAutoresizingMaskIntoConstraints  = false
+        return statusIndicator
+    }()
+    
+    // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        initConstraint()
     }
 
-
+    override func loadView() {
+        let view = UIView()
+        self.view = view
+        view.backgroundColor = .systemBackground
+        
+        [decreaseButton, increaseButton, valueLabel, activityIndicator].forEach { self.view.addSubview($0) }
+    }
+    
+    // MARK: - AutoLayout
+    func initConstraint() {
+        NSLayoutConstraint.activate([
+            decreaseButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            decreaseButton.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            decreaseButton.heightAnchor.constraint(equalTo: decreaseButton.widthAnchor),
+            
+            valueLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            valueLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            
+            increaseButton.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            increaseButton.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            increaseButton.heightAnchor.constraint(equalTo: increaseButton.widthAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            activityIndicator.topAnchor.constraint(equalTo: valueLabel.safeAreaLayoutGuide.bottomAnchor, constant: 10),
+        ])
+    }
 }
 
